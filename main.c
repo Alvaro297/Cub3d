027aa7b496ab43df1fb0cbb926cdd20be00bd352
Alvaro297 <alvaromartinfalagan29@gmail.com>
2025-07-14 18:05:50 +0200

    Quitar .o

--- main.c
+++ main.c
@@ -4,8 +4,8 @@ static void	start_cub3d(t_cub3d *cub3d)
 {
 	starting_raycasting(cub3d, cub3d->player.direction);
 	cub3d->mlx_ptr = mlx_init();
-	cub3d->win_ptr = mlx_new_window(cub3d->mlx_ptr, 1280, 720, "Cub3d");
-	cub3d->img_ptr = mlx_new_image(cub3d->mlx_ptr, 1280, 720);
+	cub3d->win_ptr = mlx_new_window(cub3d->mlx_ptr, SCREEN_WIDTH, SCREEN_HEIGHT, "Cub3d");
+	cub3d->img_ptr = mlx_new_image(cub3d->mlx_ptr, SCREEN_WIDTH, SCREEN_HEIGHT);
 	mlx_key_hook(cub3d->win_ptr, ft_key_hook, cub3d);
 	mlx_hook(cub3d->win_ptr, 6, 1L<<6, ft_mouse_hook, cub3d);
 	mlx_loop(cub3d->mlx_ptr);
