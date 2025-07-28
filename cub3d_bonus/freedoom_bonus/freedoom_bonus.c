ac0f4fcf48efa8bfa6ff444ca1f09bba51235f70
Alvaro297 <alvaromartinfalagan29@gmail.com>
2025-07-28 14:13:58 +0200

    Algo metido falta probar bonus y hacer la funcion de linerar

--- cub3d_bonus/freedoom_bonus/freedoom_bonus.c
+++ cub3d_bonus/freedoom_bonus/freedoom_bonus.c
@@ -42,10 +42,33 @@ void	ft_free_map(char **map_lines, t_cub3d *cub3d)
 	map_lines = NULL;
 }
 
+void	free_images(t_cub3d *cub3d)
+{
+	int	i;
+
+	if (cub3d->image.tex_north.img_ptr)
+		mlx_destroy_image(cub3d->mlx_ptr, cub3d->image.tex_north.img_ptr);
+	if (cub3d->image.tex_south.img_ptr)
+		mlx_destroy_image(cub3d->mlx_ptr, cub3d->image.tex_south.img_ptr);
+	i = 0;
+	while (i < TOTAL_ANIMATIONS)
+	{
+		if (cub3d->image.tex_east[i].img_ptr)
+			mlx_destroy_image(cub3d->mlx_ptr, cub3d->image.tex_east[i].img_ptr);
+		if (cub3d->image.tex_west[i].img_ptr)
+			mlx_destroy_image(cub3d->mlx_ptr, cub3d->image.tex_west[i].img_ptr);
+		i++;
+	}
+	if (cub3d->image.img_ptr)
+		mlx_destroy_image(cub3d->mlx_ptr, cub3d->image.img_ptr);
+}
+
 void	free_cub3d(t_cub3d *cub3d)
 {
 	int	i;
 
+	if (cub3d->mlx_ptr)
+		free_images(cub3d);
 	if (cub3d->map.tex_no)
 		free(cub3d->map.tex_no);
 	if (cub3d->map.tex_so)
@@ -63,6 +86,13 @@ void	free_cub3d(t_cub3d *cub3d)
 			i++;
 		}
 		free(cub3d->map.matriz);
+	}
+	if (cub3d->win_ptr)
+		mlx_destroy_window(cub3d->mlx_ptr, cub3d->win_ptr);
+	if (cub3d->mlx_ptr)
+	{
+		mlx_destroy_display(cub3d->mlx_ptr);
+		free(cub3d->mlx_ptr);
 	}
 }
 
